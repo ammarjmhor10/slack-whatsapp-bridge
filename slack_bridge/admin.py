307649c3fbfdae2cfd4dca3847a9c1d39ae851cb
ammarjmhor10 <ammarjmhor10@gmail.com>
2023-06-14 04:44:47 +0300

    add admin dashborad

--- slack_bridge/admin.py
+++ slack_bridge/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import ChatSlack
+from .models import ChatSlack,MessageSlackBridge
 # Register your models here.
 admin.site.register(ChatSlack)
+admin.site.register(MessageSlackBridge)
