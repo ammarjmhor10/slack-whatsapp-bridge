598e1a355778a5eb5709ed8ba9ff04640e72f867
ammarjmhor10 <ammarjmhor10@gmail.com>
2023-06-14 04:50:30 +0300

    edit slack bridge

--- slack_bridge/models.py
+++ slack_bridge/models.py
@@ -1,13 +1,31 @@
 from django.db import models
 from users.models import Customer
+from django.utils.translation import gettext_lazy as _
+from datetime import datetime
 # Create your models here.
 class ChatSlack(models.Model):
     channel_id = models.CharField(max_length=255,primary_key=True)
     customer  = models.ForeignKey(Customer,on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
 
-    
-class MessageSlack(models.Model):
-    ts = models.CharField(("message_id"), max_length=255,primary_key=True)
+    def __str__(self) -> str:
+        return f'{self.customer.phone_number}'
+class MessageSlackBridge(models.Model):
+    UNCOMPLETED = 'U'
+    PENDING = 'P'
+    COMPLETED = 'C'
+    SENT = 'S'
+    DELIVERED = 'D'
+    READ = 'R'
+    FAILED = 'F'
+    STATUS_CHOICES = ((COMPLETED, _('completed')),
+                       (SENT, _('sent')),
+                       (DELIVERED,_('delivered')),
+                        (READ, _('read')),
+                        (UNCOMPLETED, _('uncompleted')),
+                        (FAILED, _('failed')))
+    ts = models.CharField(("message_id"), max_length=255,unique=True)
     channel = models.ForeignKey(ChatSlack,on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(default=datetime.now())
+    message_status = models.CharField(max_length=1,choices=STATUS_CHOICES,default=COMPLETED)
+    wamid = models.TextField(verbose_name='whatsapp_message_id',null=True)
--- slack_bridge/tasks.py
+++ slack_bridge/tasks.py
@@ -1,12 +1,22 @@
 from celery import shared_task
-from .events import Message,WhatsappSenderMessage
+from . import events
+
 
 @shared_task
-def event(phone_number,message_type,body,mime_type):
+def event(phone_number,message_type,body,mime_type,ts):
+    from whatsapp.whatsapp_handler import WhatsappSenderMessage
+    from slack_bridge.models import MessageSlackBridge
     mm = None if not mime_type else mime_type
-    m = Message(message_type=message_type,body=body,meme_type=mm)
+    m = events.Message(message_type=message_type,body=body,meme_type=mm,ts=ts)
     w = WhatsappSenderMessage(phone_number=phone_number,message=m)
-    w.send_message()
-    return w
-
-
+    waid = str(w.send_message)
+    if not waid:
+        message = MessageSlackBridge.objects.get(ts=ts)
+        message.message_status = message.UNCOMPLETED
+        message.save()
+        return 'task error'
+    else:
+        message = MessageSlackBridge.objects.get(ts=ts)
+        message.wamid = w.waid
+        message.save()
+        return 'task done'
