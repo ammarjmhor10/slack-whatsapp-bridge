37df7dc35ec9caf7daafa287a965d8080f3cec09
ammarjmhor10 <ammarjmhor10@gmail.com>
2023-06-15 06:27:54 +0300

    fix bugs

--- reload.sh
+++ reload.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-sudo systemctl restart django.service
-sudo systemctl restart django.socket
-sudo systemctl restart celery.service
-sudo systemctl restart celerybeat.service
+sudo systemctl restart django.service && sudo systemctl restart django.socket && sudo systemctl restart celery.service && sudo systemctl restart celerybeat.service && echo "done"
