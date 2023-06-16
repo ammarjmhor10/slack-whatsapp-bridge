64fd62dd5420c72337ee199640fa834c1c4e6a2b
ammarjmhor10 <ammarjmhor10@gmail.com>
2023-06-16 03:32:29 +0300

    fix bugs

--- users/models.py
+++ users/models.py
@@ -1,8 +1,7 @@
 from django.db import models
-from django.contrib.auth.models import User
-from django.contrib.auth.models import AbstractUser
-from django.db import models
 from phonenumber_field.modelfields import PhoneNumberField
+# from slack_bridge.models import MessageSlackBridge
+
 # Create your models here.
 
 SEX = [
@@ -10,11 +9,15 @@ SEX = [
     ("Female",'f')
 ]
 class Customer(models.Model):
+    # from slack_bridge.models import MessageSlackBridge
     # user = models.OneToOneField(User, on_delete=models.CASCADE,null=True)
     phone_number = PhoneNumberField(unique=True,null=True)
     sex = models.CharField(max_length=10,choices=SEX,default='Male')
-    university = models.CharField(max_length=255,null=True)
+    university = models.CharField(max_length=255,null=True,blank=True)
     date_of_birth = models.DateField(null=True, blank=True)
-    
+    first_name = models.CharField(max_length=255,null=True,blank=True)
+    last_name = models.CharField(max_length=255,null=True,blank=True)
+    email = models.EmailField(null=True,blank=True)
+    message_info = models.OneToOneField(MessageSlackBridge,null=True,on_delete=models.CASCADE)
     def __str__(self) -> str:
         return f'{self.phone_number}'
--- whatsapp/tasks.py
+++ whatsapp/tasks.py
@@ -7,16 +7,19 @@ from phonenumber_field.phonenumber import phonenumbers
 from whatsapp.whatsapp_handler import WhatAppMediaExtracter
 
 @shared_task
-def send_slack_message(number,data=None):
+def send_slack_message(number,name,data=None):
     number = str('+')+number
     pn = phonenumbers.parse(number)
     s =slack.WebClient(token=os.environ.get('SLACK_TOKEN'))
     notavailble = Customer.objects.get_or_create(phone_number=pn)
     if notavailble[1]:
         r = s.conversations_create(name='n-'+str(pn.national_number))
+        customer_info = 'name On Whatsapp: '+str(name)+'\n'+'number: ' + str(pn.country_code)+str(pn.national_number) + '\n' 
         if r['ok']:
             ch = r['channel']['id']
             customer = Customer.objects 
+            pinned_message = s.chat_postMessage(channel=ch,text=customer_info)
+            pinned = s.pins_add(channel=ch,timestamp=pinned_message['ts'])
             chat = ChatSlack.objects.create(channel_id=ch,customer=notavailble[0])
             s.conversations_invite(channel=ch,users=['U04692A0Y87','U05CHHUS5CG','U05CE5ZEENR','U05CHUU129Z'])
             if data['type']=='text':
@@ -47,13 +50,19 @@ def send_slack_message(number,data=None):
 
 
 @shared_task
-def status_message(ts,status):
-    s = slack.WebClient(token=os.environ.get('SLACK_TOKEN'))
+def status_message(ts,status,number):
+    number = str('+')+number
+    pn = phonenumbers.parse(number)
+    s =slack.WebClient(token=os.environ.get('SLACK_TOKEN'))
+    notavailble = Customer.objects.get_or_create(phone_number=pn)
     m =  MessageSlackBridge.objects.get(wamid=ts)
     d = 'd'
     r = 'r'
     f = 'f'
-    ch = ChatSlack.objects.get(channel_id=str(m.channel.channel_id))
+    ch = ChatSlack.objects.get_or_create(channel_id=str(m.channel.channel_id),)
+    customer_info = 'name On Whatsapp: '+'Unknown'+'\n'+'number: ' + str(pn.country_code)+str(pn.national_number) + '\n' 
+    if notavailble[1]:
+        r = s.conversations_create(name='n-'+str(pn.national_number))
     if status == 'delivered':
         m.message_status = m.DELIVERED
         m.save()
