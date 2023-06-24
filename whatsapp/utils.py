285ea259acc2634a8be411885c886e3b3767ce2d
ammarjmhor10 <ammarjmhor10@gmail.com>
2023-06-24 16:40:39 +0300

    improve more

--- whatsapp/utils.py
+++ whatsapp/utils.py
@@ -25,7 +25,14 @@ def update_message_status(m:MessageSlackBridge,ch:ChatSlack,status,s:slack.WebCl
         return 'f'
 
 
-def send_message_slack(data,s:slack.WebClient,ch:ChatSlack):
+def send_message_slack(data,s:slack.WebClient,ch:ChatSlack,waimd:str,status=None):
+    if status:
+        m = MessageSlackBridge.objects.filter(channel=ch,wamid=waimd).first()
+        if not m: 
+            ts = s.chat_postMessage(channel=ch.channel_id,text=data)
+            m = MessageSlackBridge.objects.get_or_create(channel=ch,ts=ts['ts'],message_type=MessageSlackBridge.MARKETING,wamid=waimd)
+        r = update_message_status(m=m[0],ch=ch,s=s,status=status)
+        return 'done'
     if data['type']=='text':
         s.chat_postMessage(channel=ch.channel_id,text=data['text']['body'])
         return 'done'
@@ -38,15 +45,24 @@ def send_message_slack(data,s:slack.WebClient,ch:ChatSlack):
         rew = s.files_upload(channels=ch.channel_id,content=media.content)
         return 'done uploading'
 
-def check_chat_slack(customer:Customer,s:slack.WebClient,pn:PhoneNumber,data_message):
+def check_chat_slack(customer:Customer,s:slack.WebClient,pn:PhoneNumber,name:str,data_message,status=None,waimd=None):
     ch = ChatSlack.objects.filter(customer=customer).first()
     if not ch:
         r = s.conversations_create(name='n-'+str(pn.national_number))
-        ch = ChatSlack.objects.create(channel_id=r['channel']['id'],customer=customer)
-        name = customer.first_name
+        name = customer.nickname
         customer_info = 'name On Whatsapp: '+str(name)+'\n'+'number: ' + str(pn.country_code)+str(pn.national_number) + '\n' 
+        ch = ChatSlack.objects.create(channel_id=r['channel']['id'],customer=customer)
         pinned_message = s.chat_postMessage(channel=ch.channel_id,text=customer_info)
         pinned = s.pins_add(channel=ch.channel_id,timestamp=pinned_message['ts'])
-        s.conversations_invite(channel=ch,users=['U04692A0Y87','U05CHHUS5CG','U05CE5ZEENR','U05CHUU129Z'])
+        s.conversations_invite(channel=ch.channel_id,users=['U04692A0Y87','U05CHHUS5CG','U05CE5ZEENR','U05CHUU129Z'])
         # s.conversations_invite(channel=ch.channel_id,users=['U04692A0Y87','U05CHHUS5CG'])
-    return send_message_slack(data=data_message,s=s,ch=ch)
+        ch.message_id_info = pinned_message['ts']
+        ch.save()
+    send_message = send_message_slack(data=data_message,s=s,ch=ch,waimd=waimd,status=status)
+    if customer.nickname != name:
+        customer.nickname = name 
+        customer.save()
+        customer_info = 'name On Whatsapp: '+str(name)+'\n'+'number: ' + str(pn.country_code)+str(pn.national_number) + '\n' 
+        ts = ch.message_id_info
+        s.chat_update(channel=ch.channel_id,ts=ts,text=customer_info)
+    return ch 
