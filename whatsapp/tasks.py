42c865128f667a00edcac377abba1bdd18df5a38
ammarjmhor10 <ammarjmhor10@gmail.com>
2023-07-31 07:13:41 +0300

    add error args if it there

--- whatsapp/tasks.py
+++ whatsapp/tasks.py
@@ -31,7 +31,7 @@ def send_slack_message(number,name,data=None):
     #                                 ,data_message=data)
     #     return t 
 @shared_task
-def status_message(ts,status,number):
+def status_message(ts,status,number,error=None):
     number = str('+')+number
     pn = phonenumbers.parse(number)
     s =slack.WebClient(token=os.environ.get('SLACK_TOKEN'))
@@ -75,7 +75,7 @@ def status_message(ts,status,number):
     if not notavailble[1] and ch and m : 
         ch = ChatSlack.objects.get_or_create(customer=notavailble[0])[0]
         m = MessageSlackBridge.objects.get_or_create(channel=ch,ts=m.ts,wamid=ts)[0]
-        re = utils.update_message_status(m=m,ch=ch,status=status,s=s)
+        re = utils.update_message_status(m=m,ch=ch,status=status,s=s,error=error)
         return re 
     else:
         if status != 'sent' and not m :
@@ -85,5 +85,6 @@ def status_message(ts,status,number):
                                             ,name=c.nickname
                                             ,data_message=':mega: from US'
                                             ,status=status
-                                            ,waimd=ts)
+                                            ,waimd=ts
+                                            ,error=error)
             return chat 
