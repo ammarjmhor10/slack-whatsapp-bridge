982548b934ea124a386dd3b7960d549210888078
ammarjmhor10 <ammarjmhor10@gmail.com>
2023-09-18 13:35:35 +0300

    add site headers

--- slack_bridge/admin.py
+++ slack_bridge/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import ChatSlack,MessageSlackBridge
+import os 
 # Register your models here.
+admin.site.site_header = os.environ.get('SITE_HEADERS',default=admin.site.site_header)
 admin.site.register(ChatSlack)
 admin.site.register(MessageSlackBridge)
