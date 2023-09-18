73d7f70f1fdf8cb135d061631a1c27a90b055564
ammarjmhor10 <ammarjmhor10@gmail.com>
2023-09-18 11:31:28 +0300

    add static files

--- bridge/settings.py
+++ bridge/settings.py
@@ -207,3 +207,13 @@ STATIC_ROOT = BASE_DIR / 'static/'
 ADMIN_URL = os.environ.get('ADMIN_URL','admin')
 # AUTH_USER_MODEL = 'users.Customer'
 # CELERY_RESULT_BACKEND = "redis://localhost:6379"
+
+# Following settings only make sense on production and may break development environments.
+if not DEBUG:
+    # Tell Django to copy statics to the `staticfiles` directory
+    # in your application directory on Render.
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+    # Turn on WhiteNoise storage backend that takes care of compressing static files
+    # and creating unique names for each version so they can safely be cached forever.
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
